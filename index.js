bebfc009d06e935de4358b21df6cf5a424a0c5f5
Mavai <marko.vainio@helsinki.fi>
2018-02-03 20:31:13 +0200

    backed init

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.Promise = global.Promise
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use('/api/blogs', blogsRouter)
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
